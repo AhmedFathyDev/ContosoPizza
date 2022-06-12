84ca48873383127a534d97047f95988930d7240e
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-12 23:10:15 +0200

    Implement CRUD methods

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -1,41 +1,82 @@
+using ContosoPizza.Data;
 using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoPizza.Services;
 
 public class PizzaService
 {
-    public PizzaService()
+    private readonly PizzaContext context;
+
+    public PizzaService(PizzaContext context)
     {
-        
+        this.context = context;
     }
 
     public IEnumerable<Pizza> GetAll()
     {
-        throw new NotImplementedException();
+        return context.Pizzas.AsNoTracking().ToList();
     }
 
     public Pizza? GetById(int id)
     {
-        throw new NotImplementedException();
+        return context.Pizzas
+            .Include(p => p.Toppings)
+            .Include(p => p.Sauce)
+            .AsNoTracking().SingleOrDefault(p => p.Id == id);
     }
 
     public Pizza? Create(Pizza newPizza)
     {
-        throw new NotImplementedException();
+        context.Pizzas.Add(newPizza);
+        context.SaveChanges();
+
+        return newPizza;
     }
 
-    public void AddTopping(int PizzaId, int ToppingId)
+    public void AddTopping(int pizzaId, int toppingId)
     {
-        throw new NotImplementedException();
+        var pizzaToUpdate = context.Pizzas.Find(pizzaId);
+        var toppingToAdd = context.Toppings.Find(toppingId);
+
+        if (pizzaToUpdate is null || toppingToAdd is null)
+        {
+            throw new InvalidOperationException("Pizza or topping does not exist");
+        }
+
+        if (pizzaToUpdate.Toppings is null)
+        {
+            pizzaToUpdate.Toppings = new List<Topping>();
+        }
+
+        pizzaToUpdate.Toppings.Add(toppingToAdd);
+        
+        context.SaveChanges();
     }
 
-    public void UpdateSauce(int PizzaId, int SauceId)
+    public void UpdateSauce(int pizzaId, int sauceId)
     {
-        throw new NotImplementedException();
+        var pizzaToUpdate = context.Pizzas.Find(pizzaId);
+        var sauceToUpdate = context.Sauces.Find(sauceId);
+
+        if (pizzaToUpdate is null || sauceToUpdate is null)
+        {
+            throw new InvalidOperationException("Pizza or sauce does not exist");
+        }
+
+        pizzaToUpdate.Sauce = sauceToUpdate;
+
+        context.SaveChanges();
     }
 
     public void DeleteById(int id)
     {
-        throw new NotImplementedException();
+        var pizzaToDelete = context.Pizzas.Find(id);
+
+        if (pizzaToDelete is not null)
+        {
+            context.Pizzas.Remove(pizzaToDelete);
+            context.SaveChanges();
+        }
     }
 }
