6f8a375caa2fe3534aeb8f48e7412dd6076d040b
AhmedFathyDev <ahmed.fathy.dev@gmail.com>
2022-06-15 05:51:52 +0200

    Finish the module code

--- ContosoPizza/Program.cs
+++ ContosoPizza/Program.cs
@@ -1,4 +1,3 @@
-// Additional using declarations
 using ContosoPizza.Data;
 using ContosoPizza.Services;
 
@@ -8,10 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Add the PizzaContext
 builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
 
-// Add the PromotionsContext
 builder.Services.AddSqlite<PromotionsContext>("Data Source=./Promotions/Promotions.db");
 
 builder.Services.AddScoped<PizzaService>();
@@ -21,14 +18,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -37,7 +28,6 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-// Add the CreateDbIfNotExists method call
 app.CreateDbIfNotExists();
 
 app.Run();
